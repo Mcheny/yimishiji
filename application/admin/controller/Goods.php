e26d98805ad8df8da22858fa687075aededa1891
Mcheny <396557004@qq.com>
2017-08-28 21:44:21 +0800

    bug

--- application/admin/controller/Goods.php
+++ application/admin/controller/Goods.php
@@ -1,5 +1,10 @@
 <?php
-namespace app\admin\controller;
-class Goods extends Base{
 
+namespace app\admin\controller;
+class Goods extends Base
+{
+    function goodsList()
+    {
+    return $this->fetch('list');
+    }
 }
