b09b78e9f65a3846b72fe439efc9aea5b170adc6
Mcheny <396557004@qq.com>
2017-08-29 09:08:20 +0800

    goods

--- application/admin/controller/Order.php
+++ application/admin/controller/Order.php
@@ -1,5 +1,17 @@
 <?php
 namespace app\admin\controller;
 class Order extends Base{
+    function index()
+    {
+        return $this->fetch('list');
+    }
+    function add(){
+        return $this->fetch('add');
+    }
+    function edit(){
+        return $this->fetch('edit');
+    }
+    function del(){
 
+    }
 }
